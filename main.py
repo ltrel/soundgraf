daf105305ab6e9c55632b5b4090c949b67ff8d72
ltrel <79512656+ltrel@users.noreply.github.com>
2021-10-21 19:32:43 +1100

    Add option to cut off y-values outside a range

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sounddevice as sd
 import soundfile as sf
 import numpy as np
 import math
+from matplotlib import pyplot as plt
 
 SAMPLE_RATE = 44100
 
@@ -13,41 +14,29 @@ def safe_wrap(f):
             return y if y not in (np.inf, np.NINF) else np.nan
         except:
             return np.nan
-    
+
     return new_f
-
-
-def estimate_range(f, min_x, max_x, num_samples):
-    # Who needs calculus
-    inputs = np.linspace(min_x, max_x, num_samples)[1:]
-    min = max = safe_wrap(f)(min_x)
-
-    for x in inputs:
-        y = safe_wrap(f)(x)
-        if np.nan in (min, max):
-            min = max = y
-        elif y < min:
-            min = y
-        elif y > max:
-            max = y
-
-    return (min, max)
 
 
 def semitones_offset(base, offset):
     return base * np.power(2, offset / 12)
 
 
-def render_function(f, domain, note_range, seconds):
-    # Estimate the range of the function across the desired domain
-    # down to a resolution of one millisecond
-    f_range = estimate_range(f, domain[0], domain[1], int(seconds * 1000))
-
+def render_function(f, domain, note_range, seconds, range_limit=None):
     num_samples = int(seconds * SAMPLE_RATE)
 
     # Run the function on N inputs evenly spread over the desired domain
     f_inputs = np.linspace(domain[0], domain[1], num_samples)
     f_values = (np.vectorize(safe_wrap(f)))(f_inputs)
+
+    # Cut off any values outside of the acceptable range, this is useful
+    # for functions with vertical asymptotes
+    if range_limit != None:
+        f_values[(f_values < range_limit[0]) | (
+            f_values > range_limit[1])] = np.nan
+
+    # Estimate the range of the function, ignoring NaN values
+    f_range = (np.nanmin(f_values), np.nanmax(f_values))
 
     # Set the volume to zero whenever the function is undefined
     amplitude = np.where(np.isnan(f_values), 0.0, 1.0)
@@ -68,18 +57,25 @@ def render_function(f, domain, note_range, seconds):
 
 
 # f = lambda x: 1/8 * (x + 2) * (x - 1) * (x - 3)
-
+#
 # seconds = 2
 # domain = (-2.5, 5)
 # note_range = (-12, 12)
 
-f = lambda x: math.sqrt(-x+4)
+# f = lambda x: math.sqrt(-x+4)
+#
+# seconds = 5
+# domain = (0, 6)
+# note_range = (-24, 24)
 
-seconds = 5
-domain = (0, 6)
-note_range = (-24, 24)
+f = math.tan
 
-samples = render_function(f, domain, note_range, seconds)
+seconds = 10
+domain = (-math.pi/2, math.pi*7/2)
+note_range = (-24, 12)
+range_limit = (-6, 6)
+
+samples = render_function(f, domain, note_range, seconds, range_limit)
 
 sd.play(samples, SAMPLE_RATE)
 sd.wait()
